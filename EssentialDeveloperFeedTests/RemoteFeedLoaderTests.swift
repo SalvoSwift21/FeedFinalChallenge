5905afc1c3c6ee9c70bde17cdbf97d6f6af02022
Salvatore Milazzo <salvatore.milazzo@objectway.com>
2022-06-01 15:19:03 +0200

    Guarantee we do not deliver a result after RemoteFeedLoader have been deallocated.

--- EssentialDeveloperFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialDeveloperFeedTests/RemoteFeedLoaderTests.swift
@@ -96,6 +96,20 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_doesNotDeliverResultAfterSUTIntanceHasBeenDeallocated() {
+        let url = URL(string: "http://any-url.com")!
+        let client = HTTPClientSpy()
+        var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
+        
+        var capturedResults =  [RemoteFeedLoader.Result]()
+        sut?.load {
+            capturedResults.append($0)
+        }
+        sut = nil
+        client.complete(withStatusCode: 200, data: makeItemsJson([]))
+        XCTAssertTrue(capturedResults.isEmpty)
+    }
+    
     //MARK: - Helper
     private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!,
                          file: StaticString = #filePath,
