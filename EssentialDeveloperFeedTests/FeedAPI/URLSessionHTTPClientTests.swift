9a01776bc8a567adea9f64026778269201bc05e1
Salvatore Milazzo <salvatore.milazzo@objectway.com>
2022-06-13 15:48:56 +0200

    Delivers on expected value nil nil nil

--- EssentialDeveloperFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialDeveloperFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -15,10 +15,14 @@ class URLSessionHTTPClient {
         self.session = session
     }
     
+    struct UnexpectedValuesRepresentation: Error {}
+    
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void){
         session.dataTask(with: url) { _, _, error in
             if let error = error {
                 completion(.failure(error))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
             }
         }.resume()
     }
@@ -63,6 +67,23 @@ class URLSessionHTTPClientTests: XCTestCase {
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError, error)
+            default:
+                XCTFail("Errors")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_getFromURL_failsOnAllNilValues() {
+        URLProtocolStub.stub(data: nil, response: nil, error: nil)
+        
+        let exp = expectation(description: "Wait for")
+        makeSUT().get(from: anyURL()) { result in
+            switch result {
+            case .failure:
+                break
             default:
                 XCTFail("Errors")
             }
