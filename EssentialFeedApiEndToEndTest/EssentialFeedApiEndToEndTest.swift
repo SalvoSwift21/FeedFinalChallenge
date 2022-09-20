cac6c0ef7ae0ec5ffec411187f48004f15d70fb0
Salvatore Milazzo <salvatore.milazzo@objectway.com>
2022-09-20 17:27:21 +0200

    Use ephemeral url session configuration, for make fail test offline.

--- EssentialFeedApiEndToEndTest/EssentialFeedApiEndToEndTest.swift
+++ EssentialFeedApiEndToEndTest/EssentialFeedApiEndToEndTest.swift
@@ -9,6 +9,8 @@ import XCTest
 import EssentialDeveloperFeed
 
 class EssentialFeedApiEndToEndTest: XCTestCase {
+    
+    
 
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
         switch getFeedResult() {
@@ -36,7 +38,7 @@ class EssentialFeedApiEndToEndTest: XCTestCase {
      private func getFeedResult(file: StaticString = #filePath,
                                 line: UInt = #line) -> LoadFeedResult? {
          let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-         let client = URLSessionHTTPClient()
+         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
          let loader = RemoteFeedLoader(url: testServerURL, client: client)
          trackForMemoryLeaks(client, file: file, line: line)
          trackForMemoryLeaks(loader, file: file, line: line)
