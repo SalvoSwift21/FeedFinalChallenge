304e21fb906b881fb1ea112e23e7580022cc788b
Salvatore Milazzo <salvatore.milazzo@objectway.com>
2022-09-22 17:35:52 +0200

    Does not delete cache upon Creation

---
+++ EssentialDeveloperFeedTests/Feed Cache/CacheFeedUseCaseTest.swift
@@ -0,0 +1,28 @@
+//
+//  CacheFeedUseCaseTest.swift
+//  EssentialDeveloperFeedTests
+//
+//  Created by Salvatore Milazzo on 22/09/22.
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    init(store: FeedStore) {
+        
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+class CacheFeedUseCaseTest: XCTestCase {
+    
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+}
