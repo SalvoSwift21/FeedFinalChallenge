7750bcae8c76906b5d1b5361ed06438600c874a2
Salvatore Milazzo <salvatore.milazzo@objectway.com>
2022-09-26 18:23:31 +0200

    Move feed local to own file

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,17 @@
+//
+//  FeedStore.swift
+//  EssentialDeveloperFeed
+//
+//  Created by Salvatore Milazzo on 26/09/22.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertCompletion)
+}
+
