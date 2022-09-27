dc012a7ee76ef0b150b46a1210f13f06fc89ea56
Salvatore Milazzo <salvatore.milazzo@objectway.com>
2022-09-27 08:23:33 +0200

    add local feed items, for DTO.

--- EssentialDeveloperFeedTests/Feed Cache/CacheFeedUseCaseTest.swift
+++ EssentialDeveloperFeedTests/Feed Cache/CacheFeedUseCaseTest.swift
@@ -39,9 +39,10 @@ class CacheFeedUseCaseTest: XCTestCase {
         let timestamp = Date()
         let (sut, store) = makeSUT(currentDate: { timestamp })
         let items = [uniqueItem(), uniqueItem()]
+        let local = items.map({ LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL )})
         sut.save(items) { _ in }
         store.completeDeletionSuccessfully()
-        XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed, .insert(items, timestamp)])
+        XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed, .insert(local, timestamp)])
     }
     
     func test_save_failsOnDeltionError() {
@@ -99,7 +100,7 @@ class CacheFeedUseCaseTest: XCTestCase {
     private class FeedStoreSpy: FeedStore {
         enum ReceivedMessage: Equatable {
             case deleteCachedFeed
-            case insert([FeedItem], Date)
+            case insert([LocalFeedItem], Date)
         }
         
         private(set) var receivedMessages = [ReceivedMessage]()
@@ -119,7 +120,7 @@ class CacheFeedUseCaseTest: XCTestCase {
             deletionCompletions[index](nil)
         }
         
-        func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertCompletion) {
+        func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertCompletion) {
             insertCompletions.append(completion)
             receivedMessages.append(.insert(items, timestamp))
         }
--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -12,6 +12,20 @@ public protocol FeedStore {
     typealias InsertCompletion = (Error?) -> Void
     
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertCompletion)
+    func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertCompletion)
 }
 
+public struct LocalFeedItem: Equatable {
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+    
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+}
