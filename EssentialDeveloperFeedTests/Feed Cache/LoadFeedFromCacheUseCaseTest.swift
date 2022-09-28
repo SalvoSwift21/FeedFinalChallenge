ffcec1d8e23c06590ecaaf9bee9d43427ca74b68
Salvatore Milazzo <salvatore.milazzo@objectway.com>
2022-09-28 09:04:22 +0200

    Deliver empty cache array

--- EssentialDeveloperFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTest.swift
+++ EssentialDeveloperFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTest.swift
@@ -42,6 +42,28 @@ final class LoadFeedFromCacheUseCaseTest: XCTestCase {
         XCTAssertEqual(receivedError as NSError?, retrievalError)
     }
     
+    
+    func test_load_deliversNoImagesOnEmptyCache() {
+        let (sut, store) = makeSUT()
+        let exp = expectation(description: "Wait for save completion")
+        var receivedImages: [FeedImage]?
+        
+        sut.load { result in
+            switch result {
+            case let .success(images):
+                receivedImages = images
+            default:
+                XCTFail("Not correct at all. For result \(result)")
+            }
+            exp.fulfill()
+        }
+        
+        store.completeRetrievalWithEmptyCache()
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertEqual(receivedImages, [])
+    }
+    
     //MARK: Helpers
 
     private func makeSUT(currentDate: @escaping () -> Date = Date.init,
