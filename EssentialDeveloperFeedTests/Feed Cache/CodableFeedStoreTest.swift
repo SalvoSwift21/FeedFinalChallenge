369484176bc3428a00a9a9235efa142346cb4152
Salvatore Milazzo <salvatore.milazzo@objectway.com>
2022-10-10 16:18:17 +0200

    Move codable conform from framework agnostic LocalFeedImage to Cache store

--- EssentialDeveloperFeedTests/Feed Cache/CodableFeedStoreTest.swift
+++ EssentialDeveloperFeedTests/Feed Cache/CodableFeedStoreTest.swift
@@ -10,16 +10,39 @@ import EssentialDeveloperFeed
 
 class CodableFeedStore {
     
-    struct Cache: Codable {
-        let feed: [LocalFeedImage]
+    private struct Cache: Codable {
+        let feed: [CodableFeedImage]
         let date: Date
+        
+        var localFeed: [LocalFeedImage] {
+            return feed.map({ $0.local })
+        }
+    }
+    
+    private struct CodableFeedImage: Codable {
+        private let id: UUID
+        private let description: String?
+        private let location: String?
+        private let url: URL
+        
+        init(_ image: LocalFeedImage) {
+            id = image.id
+            description = image.description
+            location = image.location
+            url = image.url
+        }
+        
+        var local: LocalFeedImage {
+            return LocalFeedImage(id: id, description: description, location: location, url: url)
+        }
     }
     
     private let storeUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertCompletion) {
         let encoder = JSONEncoder()
-        let econded = try! encoder.encode(Cache(feed: feed, date: timestamp))
+        let cache = Cache(feed: feed.map(CodableFeedImage.init), date: timestamp)
+        let econded = try! encoder.encode(cache)
         try! econded.write(to: storeUrl)
         completion(nil)
     }
@@ -30,7 +53,7 @@ class CodableFeedStore {
         }
         let decoder = JSONDecoder()
         let cache = try! decoder.decode(Cache.self, from: data)
-        completion(.found(feed: cache.feed, timestamp: cache.date))
+        completion(.found(feed: cache.localFeed, timestamp: cache.date))
     }
 
 }
