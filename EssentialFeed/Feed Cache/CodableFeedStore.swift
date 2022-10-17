b3ec93560f60a09ddac8163bc47bde853824c75d
Salvatore Milazzo <salvatore.milazzo@objectway.com>
2022-10-17 08:32:23 +0200

    Now we use operation in parallels, but if with have side effects function, use barriers to guarantee data consistency and avoid race conditions.

--- EssentialFeed/Feed Cache/CodableFeedStore.swift
+++ EssentialFeed/Feed Cache/CodableFeedStore.swift
@@ -36,27 +36,13 @@ public class CodableFeedStore: FeedStore {
         }
     }
     
-    private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated)
+    private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated, attributes: .concurrent)
     private let storeURL: URL
     
     public init(storeUrl: URL) {
         self.storeURL = storeUrl
     }
     
-    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertCompletion) {
-        let storeURL = self.storeURL
-        queue.async {
-            do {
-                let encoder = JSONEncoder()
-                let cache = Cache(feed: feed.map(CodableFeedImage.init), date: timestamp)
-                let econded = try! encoder.encode(cache)
-                try econded.write(to: storeURL)
-                completion(nil)
-            } catch {
-                completion(error)
-            }
-        }
-    }
     
     public func retrive(completion: @escaping RetrievalCompletion) {
         let storeURL = self.storeURL
@@ -74,9 +60,24 @@ public class CodableFeedStore: FeedStore {
         }
     }
     
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertCompletion) {
+        let storeURL = self.storeURL
+        queue.async(flags: .barrier) {
+            do {
+                let encoder = JSONEncoder()
+                let cache = Cache(feed: feed.map(CodableFeedImage.init), date: timestamp)
+                let econded = try! encoder.encode(cache)
+                try econded.write(to: storeURL)
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+        }
+    }
+    
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         let storeURL = self.storeURL
-        queue.async {
+        queue.async(flags: .barrier) {
             guard FileManager.default.fileExists(atPath: storeURL.path) else {
                 return completion(nil)
             }
