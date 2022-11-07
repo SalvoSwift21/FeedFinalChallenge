069201bd405121ea1d6bfa5467126fee17532e88
Salvatore Milazzo <salvatore.milazzo@objectway.com>
2022-11-07 08:48:14 +0100

    Add test for refresh control start and end notification.

--- EssentialFeediOSTests/EssentialFeediOSTests.swift
+++ EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -67,6 +67,18 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
     }
     
+    func test_pullToRefresh_showsLoadingIndicator() {
+        let (sut, _) = makeSUT()
+        sut.refreshControl?.simulatePullToRefresh()
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
+    }
+    
+    func test_pulIToRefresh_hidesLoadingIndicatorOnLoaderCompletion() { 
+        let (sut, loader) = makeSUT()
+        sut.refreshControl?.simulatePullToRefresh()
+        loader.completeFeedLoading()
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
+    }
     //MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut:
