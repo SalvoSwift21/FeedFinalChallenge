874af8708f06b610788f7c4e902561604e22fad7
Salvatore Milazzo <salvatore.milazzo@objectway.com>
2022-11-07 10:49:17 +0100

    Move FeedViewController in Production Code.

---
+++ EssentialFeediOS/FeedViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FeedViewController.swift
+//  EssentialFeediOS
+//
+//  Created by Salvatore Milazzo on 07/11/22.
+//
+
+import Foundation
+import UIKit
+import EssentialDeveloperFeed
+
+final public class FeedViewController: UITableViewController {
+    private var loader: FeedLoader?
+    
+    public convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector (load), for: .valueChanged)
+        load()
+    }
+    
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
+    }
+}
