264561af81dc6dbfa567d89af5870c934604c9f4
Salvatore Milazzo <salvo.milazzo.21@gmail.com>
2022-12-12 08:43:19 +0100

    EssentialApp project & workspace

---
+++ EssentialApp/EssentialApp/AppDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  AppDelegate.swift
+//  EssentialApp
+//
+//  Created by Salvatore Milazzo on 12/12/22.
+//
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {}
