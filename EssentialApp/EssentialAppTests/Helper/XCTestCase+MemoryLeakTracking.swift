aeb2a250a7fbeceba0d1c3b8b1f8cc2c230bd9c6
Salvatore Milazzo <salvo.milazzo.21@gmail.com>
2022-12-13 08:46:46 +0100

    Loader Composites.

---
+++ EssentialApp/EssentialAppTests/Helper/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,17 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift
+//  EssentialAppTests
+//
+//  Created by Salvatore Milazzo on 13/12/22.
+//
+
+import Foundation
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
