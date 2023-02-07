f20d52cf954df60c5f0160e506ccb817245f26ff
Salvatore Milazzo <salvo.milazzo.21@gmail.com>
2023-02-07 09:15:17 +0100

    Feed Pagination with Caching Strategy

--- EssentialDeveloperFeed/EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialDeveloperFeed/EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -16,7 +16,7 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
             controller.dataSource.tableView(tableView, cellForRowAt: index)
         }
     }()
-    
+
     public var onRefresh: (() -> Void)?
     
     public override func viewDidLoad() {
@@ -40,7 +40,7 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
     
     public override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
+
         tableView.sizeTableHeaderToFit()
     }
     
@@ -54,17 +54,15 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
         onRefresh?()
     }
     
-    public func display(_ cellControllers: [CellController]) {
+    public func display(_ sections: [CellController]...) {
         var snapshot = NSDiffableDataSourceSnapshot<Int, CellController>()
-        snapshot.appendSections([0])
-        snapshot.appendItems(cellControllers, toSection: 0)
-        if #available(iOS 15.0, *) {
-            dataSource.applySnapshotUsingReloadData(snapshot)
-        } else {
-            dataSource.apply(snapshot)
+        sections.enumerated().forEach { section, cellControllers in
+            snapshot.appendSections([section])
+            snapshot.appendItems(cellControllers, toSection: section)
         }
+        dataSource.apply(snapshot)
     }
-    
+
     public func display(_ viewModel: ResourceLoadingViewModel) {
         refreshControl?.update(isRefreshing: viewModel.isLoading)
     }
@@ -78,14 +76,14 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
         dl?.tableView?(tableView, didSelectRowAt: indexPath)
     }
     
-    public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        let dl = cellController(at: indexPath)?.delegate
-        dl?.tableView?(tableView, didEndDisplaying: cell, forRowAt: indexPath)
-    }
-    
     public override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         let dl = cellController(at: indexPath)?.delegate
         dl?.tableView?(tableView, willDisplay: cell, forRowAt: indexPath)
+    }
+    
+    public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let dl = cellController(at: indexPath)?.delegate
+        dl?.tableView?(tableView, didEndDisplaying: cell, forRowAt: indexPath)
     }
     
     public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
